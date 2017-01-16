6cceb12de3e18b41a0f59460f8502b98ee9cea24
Ian321 <ignaz.k@live.de>
2017-01-16 20:52:32 +0100

    Minor changes

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@
 import * as got from 'got';
 var conf = require("../config");
 function doIt() {
-  console.log("New Request: "+ new Date().toISOString());
+  const newTime = new Date().toISOString();
+  console.log("New Request: " + newTime);
   got("https://icanhazip.com/").then(res => {
-    var time = new Date().toISOString();
     var TheBody = conf.body;
     TheBody.content = res.body;
-    TheBody.modified_on = time;
+    TheBody.modified_on = newTime;
 
     got.put(`https://api.cloudflare.com/client/v4/zones/${conf.zone_id}/dns_records/${conf.id}`, {
       headers: {
@@ -18,9 +18,7 @@ function doIt() {
       },
       body: JSON.stringify(TheBody).replace("\\n", "")
     }).then(res => {
-      console.log(conf.body.name+" updated!");
-    }).catch(err => {
-      console.error(err);
+      console.log(conf.body.name + " updated!");
     });
     console.log("Sending...");
   }).catch(err => {
